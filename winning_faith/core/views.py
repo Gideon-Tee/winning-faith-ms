5796c4b2c5a45940f53751568be3d95793f1ecb7
Gideon Tettehfio <gideonlambride@gmail.com>
2024-05-13 20:25:14 +0000

    user authentication

--- winning_faith/core/views.py
+++ winning_faith/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,4 +10,21 @@ def index(request):
     return render(request, 'index.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=name, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+    
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
