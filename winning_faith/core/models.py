c5f84ecfba13edc472a03e46807b18f6c5d6bacf
Gideon Tettehfio <gideonlambride@gmail.com>
2024-05-14 01:12:20 +0000

    enroll-new-student functionality

--- winning_faith/core/models.py
+++ winning_faith/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+import uuid
+from datetime import datetime
 # Create your models here.
+
+class Student(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    fname = models.CharField(max_length=25)
+    lname = models.CharField(max_length=25)
+    other_names = models.CharField(max_length=100, blank=True)
+    fees_paid = models.FloatField()
+    date_enrolled = models.DateTimeField(default=datetime.now)
+    classroom = models.CharField(max_length=25)
+    category = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
+    
--- winning_faith/core/views.py
+++ winning_faith/core/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Student
 
 # Create your views here.
 
 
-
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
+
+
+
+@login_required(login_url='login')
+def enroll(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        other_names = request.POST['other_names']
+        category = request.POST['category']
+        classroom = request.POST['classroom']
+        fees_paid = request.POST['fees_paid']
+        date_enrolled = request.POST['date_enrolled']
+
+        new_student = Student.objects.create(
+            fname = fname,
+            lname = lname,
+            other_names = other_names,
+            category = category,
+            classroom = classroom,
+            fees_paid = fees_paid,
+            date_enrolled = date_enrolled
+        )
+        new_student.save()
+        return redirect('index')
+    else:
+        return render(request, 'enroll.html')
+
+
 
 def login(request):
     if request.method == 'POST':
@@ -24,7 +55,7 @@ def login(request):
     else:
         return render(request, 'login.html')
     
-
+@login_required(login_url='login')
 def logout(request):
     auth.logout(request)
     return redirect('login')
